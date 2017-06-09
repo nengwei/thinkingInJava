4aaa5afe524e19176c45f028d35a5130ff0b65d7
neng.wei <1009002925@qq.com>
2017-06-10 00:58:37 +0800

    thread study

---
+++ Think/src/com/wn/thread/ThreadTest.java
@@ -0,0 +1,24 @@
+package com.wn.thread;
+
+/**
+ * Created by nengwei on 17/6/10.
+ */
+public class ThreadTest implements Runnable {
+
+    @Override
+    public void run() {
+        try {
+            while(true){
+                System.out.println(12);
+                Thread.sleep(1000);
+            }
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void main(String args[]){
+        Thread t = new Thread(new ThreadTest());
+        t.start();
+    }
+}
