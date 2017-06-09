49aa47d1471477b75a0f06650f7c226dbdd30c20
neng.wei <1009002925@qq.com>
2017-06-10 00:15:05 +0800

    thinkingInJava

---
+++ Think/src/com/wn/hello/Hello.java
@@ -0,0 +1,13 @@
+package com.wn.hello;
+
+/**
+ *
+ * Created by nengwei on 17/6/10.
+ */
+public class Hello {
+
+    public static void main(String args[]) {
+        System.out.println("Hello World");
+    }
+
+}
