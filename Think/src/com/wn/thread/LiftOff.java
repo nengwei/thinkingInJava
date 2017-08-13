2aadb8f5a1ebfb3e19e8746796e4a5edfe76e415
neng.wei <1009002925@qq.com>
2017-08-13 12:21:17 +0800

    thread study

---
+++ Think/src/com/wn/thread/LiftOff.java
@@ -0,0 +1,29 @@
+package com.wn.thread;
+
+/**
+ * Created by nengwei on 17/8/10.
+ */
+public class LiftOff implements Runnable {
+    protected int countDown = 10;
+    private static int taskCount = 0;
+    private final int id = taskCount++;
+
+    public LiftOff() {
+    }
+
+    public LiftOff(int countDown) {
+        this.countDown = countDown;
+    }
+
+    public String status() {
+        return "#" + id + "(" + (countDown > 0 ? countDown : "Liftoff!") + ").";
+    }
+
+    @Override
+    public void run() {
+        while (countDown-- > 0) {
+            System.out.println(status());
+            Thread.yield();//切换任务
+        }
+    }
+}
---
+++ Think/src/com/wn/thread/MainThread.java
@@ -0,0 +1,19 @@
+package com.wn.thread;
+
+/**
+ * Created by nengwei on 17/8/10.
+ */
+public class MainThread {
+
+    private static int i = 0;
+    private static final int j = i++;
+
+    public static void main(String[] args) {
+//        Thread thread = new Thread(new LiftOff());
+//        thread.start();
+        for (int i = 0; i < 5; i++) {//输出说明不同任务的执行在线程被换进换出时混在了一起。这种交换是由线程调度器自动控制的。
+            new Thread(new LiftOff()).start();
+            System.out.println("Waiting for LiftOff");
+        }
+    }
+}
