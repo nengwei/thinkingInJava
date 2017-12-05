2036d88ed4b519dc7d12e7bad53c3594564e46c5
neng.wei <1009002925@qq.com>
2017-12-05 11:22:25 +0800

    join

---
+++ Thinking/src/com/wn/thread/two/eleven/Joiner.java
@@ -0,0 +1,22 @@
+package com.wn.thread.two.eleven;
+
+public class Joiner extends Thread {
+	
+	private Sleeper sleeper;
+	
+	public Joiner(String name,Sleeper sleeper){
+		super(name);
+		this.sleeper = sleeper;
+		start();
+	}
+	
+	public void run(){
+		try {
+			sleeper.join();
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+		System.out.println(getName() + " join completed");
+	}
+
+}
