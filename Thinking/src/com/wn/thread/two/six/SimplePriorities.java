bd5454548b59167db5b277731a40639325c8acb1
neng.wei <1009002925@qq.com>
2017-12-05 09:09:39 +0800

    优先级

---
+++ Thinking/src/com/wn/thread/two/six/SimplePriorities.java
@@ -0,0 +1,50 @@
+package com.wn.thread.two.six;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+/**
+ * 优先级
+ * 
+ * @author weineng
+ *
+ * @Time 2017年12月5日
+ */
+public class SimplePriorities implements Runnable {
+
+	private int countDown = 5;
+	private volatile double d;
+	private int priority;
+
+	public SimplePriorities(int priority) {
+		super();
+		this.priority = priority;
+	}
+
+	public String toString() {
+		return Thread.currentThread() + ": " + countDown;
+	}
+
+	@Override
+	public void run() {
+		Thread.currentThread().setPriority(priority);
+		while (true) {
+			for (int i = 0; i < 100000; i++) {
+				d += (Math.PI + Math.E) / (double) i;
+				if (i % 1000 == 0)
+					Thread.yield();
+			}
+			System.out.println(this);
+			if (--countDown == 0)
+				return;
+		}
+	}
+
+	public static void main(String[] args) {
+		ExecutorService exec = Executors.newCachedThreadPool();
+		for (int i = 0; i < 5; i++)
+			exec.execute(new SimplePriorities(Thread.MIN_PRIORITY));
+		exec.execute(new SimplePriorities(Thread.MAX_PRIORITY));
+		exec.shutdown();
+	}
+}
